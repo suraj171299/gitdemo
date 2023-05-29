56a267dbe45746c39d5a7b906a8b2d5fc193d0f6
suraj171299 <surajpadalkar1999@gmail.com>
2023-05-29 11:45:59 +0530

    Usage of Local Storage

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-const btn = document.querySelector(".btn")
-const myForm = document.querySelector("#myform")
-const nameInput = document.querySelector("#name")
-btn.addEventListener("mouseout", (e)=>{
-    e.preventDefault()
-    document.querySelector("#myform").getElementsByClassName.background="#ccc"
-})
+const myForm = document.querySelector('#my-form')
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+const msg = document.querySelector('.msg')
+const userList = document.querySelector('#users')
 
-btn.addEventListener("mouseover",(e)=>{
-    e.preventDefault()
-    document.querySelector("#myform").styke.color="#ccc"
-})
-myForm.addEventListener("submit", onSubmit)
-
+myForm.addEventListener('submit', onSubmit)
 function onSubmit(e){
     e.preventDefault()
-    console.log(nameInput.value)
+
+    if(nameInput.value === '' || emailInput.value === ''){
+        msg.classList.add('error')
+        msg.innerHTML = 'Please enter all fields'
+        setTimeout(() => msg.remove(), 3000)
+    }else{
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value} `))
+
+        userList.appendChild(li)
+        const username = nameInput.value
+        const email = emailInput.value
+        localStorage.setItem('username', username)
+        localStorage.setItem('useremail', email)
+        //CLear fields
+        nameInput.value = ''
+        emailInput.value = ''
+    }
+    
 }
